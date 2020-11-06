ae92d7a3bf5874aaccf78da2ccf3dda76b6ed9c0
baophung020394 <bao.future94@gmail.com>
2020-11-06 02:44:16 -0800

    mlem

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
-require('model/database.php');
-$db = new Database;
-$db->connect();
+require 'model/database.php';
+$db = new Database();
 
-/*Xử lý các request*/
+require('controller/client/header.php'); /*giao diện header*/
 
+if (isset($_GET['controller'])) {
+    var_dump('index.php - controller');
+    require '../../route/admin/web.php'; /*xử lý các request trong Route/web.php*/
+} else {
+    var_dump('index.php - home');
+    require 'view/client/pages/home.php'; /*require giao diện trang chủ*/
+}
+
+require 'view/client/layouts/footer.php'; /*giao diện footer*/
 
 $db->closeDatabase();
